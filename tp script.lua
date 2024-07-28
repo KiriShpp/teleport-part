a49b673eebdb94285b463e5c9b9b8740f30a9305
Kiri <kirishpp@gmail.com>
2024-07-28 03:10:30 +0300

    Update and rename tp script to tp script.lua

---
+++ tp script.lua
@@ -0,0 +1,16 @@
+script.Parent.Touched:Connect(function(hit)
+	local humanoid = hit.Parent:FindFirstChildWhichIsA("Humanoid")
+	if humanoid then
+		local teleportPart = workspace:FindFirstChild("part") -- Replace "part" with the name of your teleport model
+		if teleportPart then
+			humanoid.Parent.HumanoidRootPart.CFrame = teleportPart.CFrame
+		end
+	end
+end)
+script.Parent.Touched:Connect(function(hit)
+	local humanoid = hit.Parent:FindFirstChildWhichIsA("Humanoid")
+	if humanoid then
+		print("The player has been moved") -- Logs
+		print("part1 -> part2") -- Logs ; If you want, replace the names with more convenient ones
+		end
+end)
